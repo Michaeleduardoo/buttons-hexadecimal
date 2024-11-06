0bcb793b2c65ac2553a2af6ca796a9e7e298f920
MichaelReenvie <michael@reenvie.com>
2024-11-06 15:11:19 -0300

    excluir comentarios

--- src/components/ColorName/shadesOfYellow.tsx
+++ src/components/ColorName/shadesOfYellow.tsx
@@ -1,39 +1,33 @@
 "use client";
 import "@/sass/buttons.scss";
 import { CRow, CCol } from "@coreui/react";
-import { ToastContainer, toast } from "react-toastify";
+import { toast } from "react-toastify";
 
 const copyToClipboard = (text) => {
-  // Exibe o toast de carregamento com autoClose
   const loadingToast = toast.info("Carregando...", {
     autoClose: 3000,
     isLoading: true,
   });
 
-  // Atraso para garantir que o toast de carregamento seja visível
   setTimeout(() => {
     navigator.clipboard
       .writeText(text)
       .then(() => {
-        // Fecha o toast de carregamento
         toast.dismiss(loadingToast);
 
-        // Exibe o toast de sucesso
         toast.success(`${text} Copiado.`, {
           autoClose: 3000,
         });
       })
       .catch((error) => {
-        // Fecha o toast de carregamento em caso de erro
         toast.dismiss(loadingToast);
 
-        // Exibe o toast de erro
         toast.error("Falha ao copiar para a área de transferência", {
           autoClose: 3000,
         });
         console.error(error);
       });
-  }, 2000); // Atraso de 2 segundos
+  }, 2000);
 };
 
 const Page = () => {
